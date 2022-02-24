b79325a01e7a45d530d0ba0d17193e5b5a0b7937
DEVANSH <79394737+Devansh20055@users.noreply.github.com>
2022-02-25 03:24:51 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/Devansh20055/File-Sharing-Bot'>Click here</a>\n○ Channel : @team_silent_king\n○ Support Group : @nalayaksupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/Devansh20055/File-Sharing-Bot'>Click here</a>\n○ Channel : @TEAM_SILENT_KING\n○ Support Group : @OFF_CHATS</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
